c87ffc0d18d4b20739d0e5e65023766b8277b699
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-11-05 12:18:53 +0530

    Refactored Playerchoice method

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,24 +5,37 @@ public class TicTacToeGame
 //UC-1 Create TictacToe Board
 	public static char[] board() 
 	{
-		char board_shell[]=new char[10];
-		for(int index=1;index<board_shell.length;index++) 
+		char board_cell[]=new char[10];
+		for(int index=1;index<board_cell.length;index++) 
 		{
-			board_shell[index]=' ';
+			board_cell[index]=' ';
 		}
-		return board_shell;
+		return board_cell;
 	}
 	static Scanner choice=new Scanner(System.in);
 //UC-2 Player Choosing the Letter
 	public static void  playerchoice() 
 	{
-		System.out.println("Enter your choice"
-				+ "X or"
-				+ "O");
-		char value=choice.next().charAt(0);
-		if(value=='X')
-			System.out.println("players choice is X");
+		char player;
+		char computer;
+		while(true) {
+		System.out.println("Enter your choice X or O ");
+		char value=choice.next().toUpperCase().charAt(0);
+		if(value=='X') 
+		{
+			player='X';
+			computer='O';
+			System.out.println("players choice is "+player+" and Computer choice is " +computer);
+			break;
+		}
+		else if(value=='O') {
+			player='O';
+			computer='X';
+			System.out.println(" players choice is "+player+" and Computer choice is " +computer);
+			break;
+		}
 		else
-			System.out.println("Computer choice will be O");
+			System.out.println("Invalid Input Enter your choice again");
+		}
 	}
 }
